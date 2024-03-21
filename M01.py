5e2cf82fd731513ecdf217a03b88393077418572
Eric Troutman <123651801+EricEverest@users.noreply.github.com>
2024-03-21 12:55:58 -0400

    Add files via upload

---
+++ M01.py
@@ -0,0 +1,7 @@
+seconds_per_hour = 60 * 60
+seconds_per_day = seconds_per_hour * 24
+answer1 = seconds_per_day / seconds_per_hour
+answer2 = seconds_per_day // seconds_per_hour
+
+print(answer1)
+print(answer2)
